fdf84aad53431b4e03e17ca0071d6eeccd45b0e4
Kashif Rizvi <syed.rizvi@care.com>
2018-01-21 00:23:10 +0530

    Initial Commit

---
+++ Notifications/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notifications
+//
+//  Created by Kashif Rizvi on 21/01/18.
+//  Copyright © 2018 Kashif Rizvi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
